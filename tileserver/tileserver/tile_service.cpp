e08f200b59bb0c737afdecf0acf079b6c9903c86
Tim Heuett <tim.heuett@gmail.com>
2021-03-29 21:02:15 -0700

    remove all of that error handling
    it could be done better, and isn't necessary for now

--- tileserver/tileserver/tile_service.cpp
+++ tileserver/tileserver/tile_service.cpp
@@ -11,46 +11,19 @@ TileService::TileService(const postgis::DB &db): db(db) {
 };
 
 struct Params {
-    int z;
-    float x;
-    float y;
-
-    std::vector<std::string> errors;
+    const int x, y, z;
 };
 
 const Params parseRequestParams(const http::PathParams &params) {
-    Params out;
-
-    try {
-        out.z = std::stoi(params[1]);
-    } catch (std::invalid_argument) {
-        out.errors.push_back("Invalid param `z`. Must be a whole number");
-    }
-
-    try {
-        out.x = std::stoi(params[2]);
-    } catch (std::invalid_argument) {
-        out.errors.push_back("Invalid param `x`. Must be a whole number");
-    }
-
-    try {
-        out.y = std::stoi(params[3]);
-    } catch (std::invalid_argument) {
-        out.errors.push_back("Invalid param `y`. Must be a whole number");
-    }
-
-    return out;
+    return {
+        std::stoi(params[1]),
+        std::stoi(params[2]),
+        std::stoi(params[3]),
+    };
 };
 
 void TileService::call(const http::Request &request, http::Response &response) const {
     const Params params = parseRequestParams(request.pathParams);
-    if (!params.errors.empty()) {
-        std::ostringstream errorString;
-        std::copy(params.errors.begin(), params.errors.end(), std::ostream_iterator<std::string>(errorString, "; "));
-        response.body = errorString.str();
-        response.status = http::INVALID;
-        return;
-    }
 
     response.body = "params are valid!";
 };
