f8b964b4cc344fdb616a82cee621f238c82a506a
Tim Heuett <tim.heuett@gmail.com>
2021-04-01 18:43:00 -0700

    update example query to what currently appears to be maybe working

--- example-query.sql
+++ example-query.sql
@@ -1,23 +1,13 @@
-WITH
-bounds AS (
-    SELECT ST_Segmentize(ST_MakeEnvelope(0.000000, -38.218514, 38.218514, 0.000000, 3857), 9.554629) AS geom,
-    ST_Segmentize(ST_MakeEnvelope(0.000000, -38.218514, 38.218514, 0.000000, 3857), 9.554629)::box2d as b2d
-),
-mvtgeom AS (
-    SELECT ST_AsMVTGeom(ST_Transform(t.geom, 3857), bounds.b2d) AS geom,
-        label
-    FROM land t, bounds
-    WHERE ST_Intersects(t.geom, ST_Transform(bounds.geom, 26918))
-)
-SELECT ST_AsMVT(mvtgeom.*) FROM mvtgeom;
-
-
-SELECT ST_AsMVT(q, 'pois', 4096, 'geom')
+SELECT ST_AsMVT(q, 'poi')
 FROM (
     SELECT
-        label,
+        gid, label,
         ST_AsMVTGeom(
             geom,
-            ST_MakeEnvelope
-        )
-)
+            ST_Segmentize(ST_MakeEnvelope(9.554629, 0.000000, 19.109257, 9.554629, 3857), 2.388657),
+            4096,
+            256,
+            false
+        ) geom
+    FROM poi
+) q;
